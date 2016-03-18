f9d585f16ef954dfd103656bd642297247331532
FrayedSanity <thought_chasm@yahoo.com>
2016-03-17 19:43:02 -0700

    BlinkRampUpDown
    This sketch focused on the use of if statements change the state of the LED and change it's flash frequency by ramping it up and down continuously while displaying the current frequency on the PC through serial communication.

---
+++ BlinkRampUpDown.ino
@@ -0,0 +1,67 @@
+/*This program is an introduction to the "if" statement.  In this program
+The onboard PIN 13 LED Will start flashing at a starting frequency and ramp
+down to a threshold frequency and then ramp up to a threshold frequency.
+This oscillation will be continuous.
+
+Developer: Wes Urbat
+Last Modified: 03.17.16*/
+
+
+//Initial global variable declaration and assignment
+int freq = 1000; //defines the frequency that the LED will be flashed.
+int freqRamp = 100; //defines the frequency ramp rate +/- per scan.
+int freqMin = 0; //defines the minimum frequency in ms.
+int freqMax = 2000; //defines the maximum frequency in ms.
+int Pin = 13; //defines the pin the LED will be connected to
+bool minReached = false; //trigger for min freq reached.
+int LedState = LOW; //Definies the initial state of the LED HIGH/LOW (on or off) 
+
+void setup()
+{
+	
+	//Initialize and configure hardware
+	Serial.begin(9600); //This opens the serial commuincation at a baud rate of 9600bps bits per second.
+	pinMode(Pin, OUTPUT); //defines the specified PIN as an output
+	
+}
+
+void loop()
+{
+	//Determines current LED state and sets to opposite then writes the output
+	//to the PIN and delays by the defined frequency
+
+	if (LedState == LOW)
+	{
+		LedState = HIGH;
+	}
+	else
+	{
+		LedState = LOW;
+	}
+	digitalWrite(Pin, LedState);
+	delay(freq);
+
+	//Determines if the current frequency is less or equal to Minimum frequency
+	//or greater than or equal to the maximum frequency, and in return causing the
+	//frequency of the LED flash to ramp up or down.
+	if (!minReached)
+	{
+		freq = freq - freqRamp;
+		Serial.println(freq); //Outputs current frequency in ms to the USB to the PC's monitor.
+
+		if (freq <= freqMin)
+		{
+			minReached = true;
+		}
+		else{}
+	}
+	else
+	{
+		freq = freq + freqRamp;
+		Serial.println(freq); //Outputs current frequency in ms to the USB to the PC's monitor.
+		if (freq >= freqMax)
+		{
+			minReached = false;
+		}
+	}
+}
